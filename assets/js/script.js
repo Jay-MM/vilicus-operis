644e71201ca46ceca1c7d5ffc67bb460ee65fa8a
Jesus M <marquez.jay444@gmail.com>
2023-12-02 23:07:40 -0600

    migrates code from moment to day.js and creates function that handles appending data into DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,33 @@ const projectTypeInput = $('#project-type-input');
 const hourlyRateInput = $('#hourly-rate-input');
 const dueDateInput = $('#due-date-input')
 const projectModal = $('#project-modal')
+const projectDisplay = $('#project-display')
 
+const today = dayjs()
 
+// handle appending data unto DOM
 function renderProjectData(name, type, hourlyRate, dueDate) {
-  console.log(name, type, hourlyRate, dueDate)
   // show data in table
     // create a tr(table row)
-    // create td(cell) for each piece of data
-    // append tds into tr
+  const tr = $('<tr>')
+
+    // create td(cell) for each piece of data && append tds into tr
+  $('<td>').text(name).appendTo(tr);
+  $('<td>').text(type).appendTo(tr);
+  $('<td>').text('$' + hourlyRate).appendTo(tr);
+  $('<td>').text(dueDate).appendTo(tr);
+
+  
+  // td for days until dueDate 
+  const daysLeft = dayjs(dueDate).diff(today, 'day')
+  $('<td>').text(daysLeft).appendTo(tr);
+  
+    // td for  potential earnings ($ = hourlyRate * 8)
+const potentialEarnings = (parseFloat(hourlyRate) * 8) * daysLeft
+  $('<td>').text('$' + potentialEarnings).appendTo(tr);
+
     // append into tbody
+  projectDisplay.append(tr);
 }
 // handleSubmit from modal form
 function handleSubmit(e) {
@@ -25,13 +43,15 @@ function handleSubmit(e) {
   const hourlyRate = hourlyRateInput.val().trim()
   const dueDate = dueDateInput.val().trim()
 
-  
-
-  renderProjectData(name, type, hourlyRate, dueDate)
-  
+  // Check if dueDate is a valid date
+  if (dayjs(dueDate).isValid()) {
+    renderProjectData(name, type, hourlyRate, dueDate)
+  } else {
+    console.error(`Invalid date: ${dueDate} (use mm-dd-yy format)`);
+  }
 
   // hide modal
-projectModal.modal('hide')
+  projectModal.modal('hide')
 
   // clear inputs
   projectNameInput.val('')
@@ -39,17 +59,15 @@ projectModal.modal('hide')
   hourlyRateInput.val('')
   dueDateInput.val('')
 }
-
 // add event listners 
 projectForm.on('submit', handleSubmit);
 // handle current time display
-currentTime.text(moment().format("ddd , MMMM Do YYYY :: h:mm:ss a"))
+currentTime.text(today.format("ddd , MMMM DD YYYY :: h:mm:ss a"))
 
 // datepicker using jqueryUI
 dueDateInput.datepicker({
   showAnim: "slideDown",
-  dateFormat: "y-mm-d",
-  minDate: 1, 
+  dateFormat: "mm-dd-yy",
+  minDate:1, 
 });
-// handle parsing data unto DOM
-// handle project deletion 
+// handle project deletion
