f4835ef65a2a81716ec13a2b4cac2368e8a74be5
Jesus M <marquez.jay444@gmail.com>
2023-12-03 00:45:02 -0600

    adds delete functionality && adds finishing touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,23 +17,36 @@ function renderProjectData(name, type, hourlyRate, dueDate) {
   const tr = $('<tr>')
 
     // create td(cell) for each piece of data && append tds into tr
-  $('<td>').text(name).appendTo(tr);
-  $('<td>').text(type).appendTo(tr);
-  $('<td>').text('$' + hourlyRate).appendTo(tr);
-  $('<td>').text(dueDate).appendTo(tr);
-
+  $('<td>').addClass('text-center').text(name).appendTo(tr);
+  $('<td>').addClass('text-center').text(type).appendTo(tr);
+  $('<td>').addClass('text-center').text('$' + hourlyRate).appendTo(tr);
+  $('<td>').addClass('text-center').text(dueDate).appendTo(tr);
   
   // td for days until dueDate 
   const daysLeft = dayjs(dueDate).diff(today, 'day')
-  $('<td>').text(daysLeft).appendTo(tr);
+  $('<td>').addClass('text-center').text(daysLeft).appendTo(tr);
   
-    // td for  potential earnings ($ = hourlyRate * 8)
-const potentialEarnings = (parseFloat(hourlyRate) * 8) * daysLeft
-  $('<td>').text('$' + potentialEarnings).appendTo(tr);
+  // td for  potential earnings ($ = hourlyRate * 8)
+  const potentialEarnings = (parseFloat(hourlyRate) * 8) * daysLeft
+  $('<td>').addClass('text-center').text('$' + potentialEarnings).appendTo(tr);
+  
+  // delete button
+  const deleteProjectBtnn = $('<td>').addClass('p-2 delete-project-btn text-center').text('X').appendTo(tr);
 
-    // append into tbody
+  // append into tbody
   projectDisplay.append(tr);
+
+
+
+
 }
+// handle project deletion
+function handleDeleteProject(e) {
+  console.log(e.target);
+  const btnClicked = $(e.target);
+  btnClicked.closest('tr').remove();
+}
+
 // handleSubmit from modal form
 function handleSubmit(e) {
   e.preventDefault()
@@ -59,8 +72,6 @@ function handleSubmit(e) {
   hourlyRateInput.val('')
   dueDateInput.val('')
 }
-// add event listners 
-projectForm.on('submit', handleSubmit);
 // handle current time display
 currentTime.text(today.format("ddd , MMMM DD YYYY :: h:mm:ss a"))
 
@@ -70,4 +81,6 @@ dueDateInput.datepicker({
   dateFormat: "mm-dd-yy",
   minDate:1, 
 });
-// handle project deletion
+// add event listners 
+projectForm.on('submit', handleSubmit);
+projectDisplay.on('click', '.delete-project-btn', handleDeleteProject);
